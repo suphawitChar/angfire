11e51e91e4edb9fb15375cf73c9cd66749f2afa6
suphawit19 <suphawit1942@gmail.com>
2022-01-23 17:02:52 +0700

    commit1

---
+++ src/environments/firebase-config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAPwzutTffKkvnjuWvlZYsW5fRaWsyfx74",
+  authDomain: "angular-firebase-d9ce1.firebaseapp.com",
+  projectId: "angular-firebase-d9ce1",
+  storageBucket: "angular-firebase-d9ce1.appspot.com",
+  messagingSenderId: "1027418878600",
+  appId: "1:1027418878600:web:2aa0c30ca5e5861a88a53d",
+  measurementId: "G-Y79EK56CMF"
+};
