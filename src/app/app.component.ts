91a6e60c09d7e945ddcf90efa5fd1c43af9db260
suphawit19 <suphawit1942@gmail.com>
2022-01-24 16:13:11 +0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/compat/firestore";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'angfire';
+  dataForm: FormGroup = this.formBuilder.group({
+    name: null,
+    age: null,
+    address: null,
+    id: null,
+  });
+  myArray: any;
+
+  constructor(
+    private firestore: AngularFirestore,
+    private formBuilder: FormBuilder
+  ) {
+    this.myArray = this.firestore
+      .collection("items", (ref) => ref.orderBy("id", "asc"))
+      .valueChanges();
+  }
+
+  onSubmit() {
+    const data = this.firestore.collection('items', (ref) =>
+      ref.where('id', '==', this.dataForm.value.id)
+    );
+
+    data.get().subscribe((res) => {
+      if (res.docs.length != 0) {
+        res.docs[0].ref.update(this.dataForm.value);
+      } else {
+        this.firestore.collection('items').add(this.dataForm.value);
+      }
+    });
+  }
+
+  onEdit(doc: any) {
+    this.dataForm = this.formBuilder.group({
+      name: doc.name,
+      age: doc.age,
+      address: doc.address,
+      id: doc.id,
+    });
+  }
+
+  onDelete(id: string, name: string) {
+    if (confirm('Do you want to delete '+name+'?')) {
+      const data = this.firestore.collection('items', (ref) =>
+        ref.where('id', '==', id)
+      );
+
+      data.get().subscribe((res) => {
+        if (res.docs.length != 0) {
+          res.docs[0].ref.delete();
+        }
+      });
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//step 1
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
+import { firebaseConfig } from 'src/environments/firebase-config';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    //step2
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule, // firestore
+    AngularFireDatabaseModule, // Database
   ],
   providers: [],
   bootstrap: [AppComponent]
